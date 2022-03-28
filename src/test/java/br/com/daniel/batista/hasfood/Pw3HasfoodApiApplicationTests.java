485b7b8c5327e4c2059f3c112f5c87a8b812ff8a
danbatistaskt <danbatistask8@gmail.com>
2022-03-28 08:02:53 -0300

    Criação de projeto

---
+++ src/test/java/br/com/daniel/batista/hasfood/Pw3HasfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daniel.batista.hasfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw3HasfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
